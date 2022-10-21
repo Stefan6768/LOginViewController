5e283a76272d26a9679d8a2f2475a885da531724
Stefan <103416259+Stefan6768@users.noreply.github.com>
2022-10-21 10:43:44 +0200

    Initial Commit

---
+++ LOginViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOginViewController
+//
+//  Created by Stefan Schreiber on 21.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
